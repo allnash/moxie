ce3410ee7a1f741c2c57005ced7417cabbea3e5c
Nash Gadre <nash8@live.com>
2023-08-07 10:17:54 -0400

    update go sum

--- config/config.go
+++ config/config.go
@@ -1,18 +1,18 @@
 package config
 
 type Config struct {
-    Version         string    `yaml:"version"`
-    StatusHost      string    `yaml:"status_host"`
-    ProxyListenPort string    `yaml:"proxy_listen_port"`
-    Logfile         string    `yaml:"log_file"`
-    Services        []Service `yaml:"services"`
+	Version         string    `yaml:"version"`
+	StatusHost      string    `yaml:"status_host"`
+	ProxyListenPort string    `yaml:"proxy_listen_port"`
+	Logfile         string    `yaml:"log_file"`
+	Services        []Service `yaml:"services"`
 }
 
 type Service struct {
-    Name          string `yaml:"name"`
-    Type          string `yaml:"type"` // Type is one of ['web', 'proxy', 'static']
-    IngressUrl    string `yaml:"ingress_url"`
-    EgressUrl     string `yaml:"egress_url"`
-    XFrameOptions string `yaml:"x_frame_options" env-default:"SAMEORIGIN"` // XFrameOptions is one of ['DENY', 'SAMEORIGIN', 'ALLOW-FROM']
-    HSTSMaxAge    int    `yaml:"hsts_max_age" env-default:"5184000"`       // HSTSMaxAge is the max age in seconds
+	Name          string `yaml:"name"`
+	Type          string `yaml:"type"` // Type is one of ['web', 'proxy', 'static']
+	IngressUrl    string `yaml:"ingress_url"`
+	EgressUrl     string `yaml:"egress_url"`
+	XFrameOptions string `yaml:"x_frame_options" env-default:"SAMEORIGIN"` // XFrameOptions is one of ['DENY', 'SAMEORIGIN', 'ALLOW-FROM']
+	HSTSMaxAge    int    `yaml:"hsts_max_age" env-default:"5184000"`       // HSTSMaxAge is the max age in seconds
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/allnash/moxie
 go 1.16
 
 require (
-	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/jpillora/ipfilter v1.2.8
 	github.com/labstack/echo/v4 v4.5.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
